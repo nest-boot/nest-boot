2709cb84a8cdd426e14a7f7e5ea1ce20a1adc4d8
d4rkcr0w <me@d4rkcr0w.com>
2022-01-18 17:50:40 +0800

    fix: fix release

--- .releaserc.js
+++ .releaserc.js
@@ -10,8 +10,9 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
     "@semantic-release/npm",
-    "@semantic-release/git",
     "@semantic-release/github",
+    "@semantic-release/git",
   ],
 };
