e30eb305319d2b0100ea848872f99480b3b0d94e
d4rkcr0w <me@d4rkcr0w.com>
2022-01-20 16:02:32 +0800

    fix: fix dependencies

--- packages/logger/src/default-pino-options.ts
+++ packages/logger/src/default-pino-options.ts
@@ -1,6 +1,6 @@
+import { randomUUID } from "crypto";
 import { Options } from "pino-http";
-import * as uuid from "uuid";
 
 export const defaultPinoOptions: Options = {
-  genReqId: () => uuid.v4(),
+  genReqId: () => randomUUID(),
 };
