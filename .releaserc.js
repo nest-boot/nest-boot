93dc335a16e6f2200d326512e03344ff801ab263
d4rkcr0w <me@d4rkcr0w.com>
2022-04-19 16:31:55 +0800

    fix: update

--- .releaserc.js
+++ .releaserc.js
@@ -15,7 +15,7 @@ module.exports = {
     [
       "@semantic-release/git",
       {
-        assets: ["package.json"],
+        assets: ["package.json", "package-lock.json"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
