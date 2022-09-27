5203cd92203ab38fabafb1bb58cc6783f610d80d
d4rkcr0w <me@d4rkcr0w.com>
2022-09-27 16:01:22 +0800

    fix: databaseConfig

--- packages/database/src/utils/database-config.util.ts
+++ packages/database/src/utils/database-config.util.ts
@@ -4,23 +4,18 @@ import { TsMorphMetadataProvider } from "@mikro-orm/reflection";
 import { MigrationGenerator } from "../migration-generator";
 
 export function databaseConfig<D extends IDatabaseDriver<Connection>>(
-  options?: Options<D>
+  options: Options<D> = {}
 ): Options<D> {
   return {
     debug:
-      options?.debug ||
-      !!process.env.DATABASE_DEBUG ||
-      process.env.NODE_ENV !== "production",
-    type: options?.type || (process.env.DATABASE_TYPE as Options<D>["type"]),
-    host: options?.host || process.env.DATABASE_HOST,
-    port: options?.port || +process.env.DATABASE_PORT,
-    name: options?.name || process.env.DATABASE_USERNAME,
-    password: options?.password || process.env.DATABASE_PASSWORD,
-    timezone: options?.timezone || process.env.DATABASE_TIMEZONE || process.env.TZ,
-    clientUrl:
-      options?.clientUrl ||
-      `${process.env.DATABASE_TYPE}://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}?application_name=${process.env.APP_NAME}`,
-    dbName: options?.dbName || process.env.DATABASE_NAME,
+      !!process.env.DATABASE_DEBUG || process.env.NODE_ENV !== "production",
+    type: process.env.DATABASE_TYPE as Options<D>["type"],
+    host: process.env.DATABASE_HOST,
+    port: +process.env.DATABASE_PORT,
+    name: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    timezone: process.env.DATABASE_TIMEZONE || process.env.TZ,
+    dbName: process.env.DATABASE_NAME,
     metadataProvider: TsMorphMetadataProvider,
     entities: ["dist/**/*.entity.js"],
     entitiesTs: ["src/**/*.entity.ts"],
@@ -36,5 +31,6 @@ export function databaseConfig<D extends IDatabaseDriver<Connection>>(
       defaultSeeder: "Seeder",
       fileName: (className: string) => className,
     },
+    ...options,
   };
 }
