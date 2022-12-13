1b7e87c0927b30ba5557b8b4daa203becf8bf9d9
D4rkCr0w <me@d4rkcr0w.com>
2022-12-14 00:14:23 +0800

    chore: 修正语义化版本发布

--- .releaserc.js
+++ .releaserc.js
@@ -10,21 +10,7 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/npm",
-      {
-        "npmPublish": false
-      }
-    ],
-    // "@semantic-release/github",
-    // [
-    //   "@semantic-release/git",
-    //   {
-    //     assets: ["package.json", "pnpm-lock.yaml"],
-    //     message:
-    //       "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-    //   },
-    // ],
+    "@semrel-extra/npm"
   ],
   noCi: true,
 };
