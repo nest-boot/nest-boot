a10673bbdf712d98e416900a832c4d661540d4eb
chenxuanzong <48152698+chenxuanzong@users.noreply.github.com>
2023-03-30 15:57:49 +0800

    fix: redis 模块补充 super 调用 (#9)
    * fix: redis 模块补充 super 调用

--- packages/redis/src/redis.module.ts
+++ packages/redis/src/redis.module.ts
@@ -21,7 +21,9 @@ export class RedisModule
   extends ConfigurableModuleClass
   implements OnApplicationShutdown
 {
-  constructor(private readonly redis: Redis) {}
+  constructor(private readonly redis: Redis) {
+    super();
+  }
 
   async onApplicationShutdown(): Promise<void> {
     await this.redis.quit();
