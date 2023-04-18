ee93b11c04a0d90100ce48844a6d4c799f97a3c4
D4rkCr0w <me@d4rkcr0w.com>
2023-04-18 16:42:43 +0800

    fix(graphql): 游标支持传递 null

--- packages/graphql/src/utils/get-paging-type.ts
+++ packages/graphql/src/utils/get-paging-type.ts
@@ -1,19 +1,19 @@
 import { PagingType } from "../enums";
-import { ConnectionArgsInterface } from "../interfaces";
+import { type ConnectionArgsInterface } from "../interfaces";
 
 export function getPagingType(
   connectionArgs: ConnectionArgsInterface<any, any>
 ): PagingType {
   const { first, last, after, before } = connectionArgs;
   const isForwardPaging =
-    typeof first !== "undefined" || typeof after !== "undefined";
+    typeof first === "number" || typeof after === "string";
   const isBackwardPaging =
-    typeof last !== "undefined" || typeof before !== "undefined";
+    typeof last === "number" || typeof before === "string";
 
   if (isForwardPaging && isBackwardPaging) {
     if (
-      (isForwardPaging && typeof before !== "undefined") ||
-      (isBackwardPaging && typeof after !== "undefined")
+      (isForwardPaging && before != null) ||
+      (isBackwardPaging && after != null)
     ) {
       throw new Error("paging must use either first/after or last/before");
     } else {
