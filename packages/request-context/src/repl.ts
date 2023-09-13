c4dca7be0cb09da5ba7c810949c54250c8d0ec02
D4rkCr0w <me@d4rkcr0w.com>
2023-09-13 23:02:47 +0800

    fix(request-context): 修复在 REPL 下有可能会丢失上下文。

--- packages/request-context/src/repl.ts
+++ packages/request-context/src/repl.ts
@@ -6,6 +6,8 @@ import { REPL_INITIALIZED_MESSAGE } from "@nestjs/core/repl/constants";
 import { ReplContext } from "@nestjs/core/repl/repl-context";
 import { ReplLogger } from "@nestjs/core/repl/repl-logger";
 import { defineDefaultCommandsOnRepl } from "@nestjs/core/repl/repl-native-commands";
+import { AsyncResource } from "async_hooks";
+import { Transform } from "stream";
 
 import { RequestContext } from "./request-context";
 
@@ -14,18 +16,38 @@ export async function repl(module: Type | DynamicModule) {
     abortOnError: false,
     logger: new ReplLogger(),
   });
+
   await app.init();
 
+  const replContext = new ReplContext(app);
+  Logger.log(REPL_INITIALIZED_MESSAGE);
+
   return await RequestContext.run(new RequestContext(), async () => {
-    const replContext = new ReplContext(app);
-    Logger.log(REPL_INITIALIZED_MESSAGE);
+    const asyncResource = new AsyncResource("REPL");
 
     const replServer = (await import("repl")).start({
+      input: new Proxy(
+        process.stdin.pipe(
+          new Transform({
+            transform(chunk, encoding, callback) {
+              asyncResource.runInAsyncScope(callback, null, null, chunk);
+            },
+          }),
+        ),
+        {
+          get(target, prop, receiver) {
+            if (prop in target) {
+              return Reflect.get(target, prop, receiver);
+            } else if (prop in process.stdin) {
+              return Reflect.get(process.stdin, prop, receiver);
+            }
+          },
+        },
+      ),
+      output: process.stdout,
       prompt: clc.green("> "),
       ignoreUndefined: true,
     });
-
-    replServer.context.RequestContext = RequestContext;
 
     assignToObject(replServer.context, replContext.globalScope);
 
