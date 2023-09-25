c8aa3f9cbf6867b5264829c2bb0da9332bd67d4a
D4rkCr0w <me@d4rkcr0w.com>
2023-09-25 17:50:51 +0800

    fix(RequestContext): 修改请求上下文拦截器实现逻辑

--- packages/request-context/src/request-context.interceptor.ts
+++ packages/request-context/src/request-context.interceptor.ts
@@ -4,24 +4,34 @@ import {
   Injectable,
   type NestInterceptor,
 } from "@nestjs/common";
-import { from, lastValueFrom, type Observable } from "rxjs";
+import { Observable } from "rxjs";
 
 import { RequestContext } from "./request-context";
 
 @Injectable()
 export class RequestContextInterceptor implements NestInterceptor {
   intercept<T>(context: ExecutionContext, next: CallHandler<T>): Observable<T> {
-    if (context.getType() === "rpc") {
-      const ctx = new RequestContext();
-
-      return from(
-        RequestContext.run<T>(
-          ctx,
-          async () => await lastValueFrom(next.handle()),
-        ),
-      );
+    if (RequestContext.isActive()) {
+      return next.handle();
     }
 
-    return next.handle();
+    const ctx = new RequestContext();
+
+    return new Observable((subscriber) => {
+      void RequestContext.run(ctx, () => {
+        try {
+          next
+            .handle()
+            .pipe()
+            .subscribe({
+              next: (res) => { subscriber.next(res); },
+              error: (err) => { subscriber.error(err); },
+              complete: () => { subscriber.complete(); },
+            });
+        } catch (err) {
+          subscriber.error(err);
+        }
+      });
+    });
   }
 }
