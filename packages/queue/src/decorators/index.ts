a20549449615b6c82d24a31668171f23224e220b
D4rkCr0w <me@d4rkcr0w.com>
2023-11-08 17:53:36 +0800

    feat: add legacyProcessor support

--- packages/queue/src/decorators/index.ts
+++ packages/queue/src/decorators/index.ts
@@ -1,3 +1,4 @@
 export * from "./consumer.decorator";
 export * from "./inject-queue.decorator";
+export * from "./legacy-processor.decorator";
 export * from "./processor.decorator";
--- packages/queue/test/src/app.module.ts
+++ packages/queue/test/src/app.module.ts
@@ -4,8 +4,10 @@ import { ConfigModule, ConfigService } from "@nestjs/config";
 import { QueueModule } from "../../src";
 import { TEST_QUEUE_NAME, TEST_REQUEST_SCOPED_QUEUE_NAME } from "./constants";
 import { TestConsumer } from "./test.consumer";
+import { TestLegacyProcessor } from "./test.legacy-processor";
 import { TestProcessor } from "./test.processor";
 import { TestRequestScopedConsumer } from "./test-request-scoped.consumer";
+import { TestRequestScopedLegacyProcessor } from "./test-request-scoped.legacy-processor";
 import { TestRequestScopedProcessor } from "./test-request-scoped.processor";
 
 @Module({
@@ -49,9 +51,11 @@ import { TestRequestScopedProcessor } from "./test-request-scoped.processor";
   ],
   providers: [
     TestConsumer,
-    TestRequestScopedConsumer,
     TestProcessor,
+    TestLegacyProcessor,
+    TestRequestScopedConsumer,
     TestRequestScopedProcessor,
+    TestRequestScopedLegacyProcessor,
   ],
 })
 export class AppModule {}
