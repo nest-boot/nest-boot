a1f5629763a9bb5ab48c1f711318cf20054abe0e
D4rkCr0w <me@xudong.cc>
2023-11-29 14:50:55 +0800

    fix(graphql): duration changed to nanoseconds
    * feat(graphql): graphql add logger plugin
    
    * fix(graphql): duration changed to nanoseconds
    
    ---------
    
    Co-authored-by: D4rkCr0w <me@d4rkcr0w.com>

--- packages/graphql/src/plugins/logging.plugin.ts
+++ packages/graphql/src/plugins/logging.plugin.ts
@@ -9,6 +9,10 @@ import { Logger } from "@nest-boot/logger";
 import { Plugin } from "@nestjs/apollo";
 import { Optional } from "@nestjs/common";
 
+function durationHrTimeToNanos(hrtime: [number, number]) {
+  return hrtime[0] * 1e9 + hrtime[1];
+}
+
 @Plugin()
 export class LoggingPlugin implements ApolloServerPlugin {
   constructor(
@@ -20,30 +24,34 @@ export class LoggingPlugin implements ApolloServerPlugin {
     const logger = this.logger;
 
     if (typeof logger !== "undefined") {
-      const startTime = Date.now();
+      const startHrTime = process.hrtime();
 
       return {
         didResolveOperation: async (ctx) => {
-          if (typeof this.logger !== "undefined") {
-            logger.assign({
-              operation: {
-                id: ctx.queryHash,
-                name: ctx.operationName,
-                type: ctx.operation?.operation?.toUpperCase(),
-              },
-            });
-          }
+          logger.assign({
+            operation: {
+              id: ctx.queryHash,
+              name: ctx.operationName,
+              type: ctx.operation?.operation?.toUpperCase(),
+            },
+          });
         },
-        willSendResponse: async (ctx) => {
-          const executionTime = Date.now() - startTime;
+        willSendResponse: async ({ response, errors }) => {
+          const durationNs = durationHrTimeToNanos(process.hrtime(startHrTime));
 
-          if (ctx.errors) {
-            ctx.errors.forEach((err) => {
-              logger.error(err.message, { err });
-            });
+          if (response.body.kind === "incremental") {
+            return;
           }
 
-          logger.log("resolve operation completed", { executionTime });
+          if (typeof errors !== "undefined" && errors.length > 0) {
+            logger.error("graphql request errored", {
+              durationNs,
+            });
+          } else {
+            logger.log("graphql request completed", {
+              durationNs,
+            });
+          }
         },
       };
     }
