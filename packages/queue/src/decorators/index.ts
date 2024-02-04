7bae568718b03257d0623869c68881ee0dd8d887
Xudong Huang <me@xudong.cc>
2024-02-04 08:59:20 +0000

    fix(queue): remove legacy processor decorator and related code

--- packages/queue/src/decorators/index.ts
+++ packages/queue/src/decorators/index.ts
@@ -1,4 +1,3 @@
 export * from "./consumer.decorator";
 export * from "./inject-queue.decorator";
-export * from "./legacy-processor.decorator";
 export * from "./processor.decorator";
--- packages/queue/test/src/app.module.ts
+++ packages/queue/test/src/app.module.ts
@@ -4,10 +4,8 @@ import { ConfigModule, ConfigService } from "@nestjs/config";
 import { QueueModule } from "../../src";
 import { TEST_QUEUE_NAME, TEST_REQUEST_SCOPED_QUEUE_NAME } from "./constants";
 import { TestConsumer } from "./test.consumer";
-import { TestLegacyProcessor } from "./test.legacy-processor";
 import { TestProcessor } from "./test.processor";
 import { TestRequestScopedConsumer } from "./test-request-scoped.consumer";
-import { TestRequestScopedLegacyProcessor } from "./test-request-scoped.legacy-processor";
 import { TestRequestScopedProcessor } from "./test-request-scoped.processor";
 
 @Module({
@@ -52,10 +50,8 @@ import { TestRequestScopedProcessor } from "./test-request-scoped.processor";
   providers: [
     TestConsumer,
     TestProcessor,
-    TestLegacyProcessor,
     TestRequestScopedConsumer,
     TestRequestScopedProcessor,
-    TestRequestScopedLegacyProcessor,
   ],
 })
 export class AppModule {}
