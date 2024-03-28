a2ff6d7d92cf661b1585e25a24e279df3f723e5a
Xudong Huang <me@xudong.cc>
2024-03-28 17:27:04 +0800

    Supports AUTH_USER and AUTH_PERSONAL_ACCESS_TOKEN aliases (#73)

--- packages/auth/src/auth.module-definition.ts
+++ packages/auth/src/auth.module-definition.ts
@@ -9,3 +9,7 @@ export const REQUIRE_AUTH_METADATA_KEY = randomUUID();
 
 export const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN } =
   new ConfigurableModuleBuilder<AuthModuleOptions>().build();
+
+export const AUTH_USER = Symbol("AUTH_USER");
+
+export const AUTH_PERSONAL_ACCESS_TOKEN = Symbol("AUTH_PERSONAL_ACCESS_TOKEN");
