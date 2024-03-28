0dad117072f4e8b93ef551dfe2a5819704f62113
Xudong Huang <me@xudong.cc>
2024-03-28 21:34:01 +0800

    Fix decorator failure when using custom entities

--- packages/auth/src/auth.module-definition.ts
+++ packages/auth/src/auth.module-definition.ts
@@ -1,15 +1,6 @@
 import { ConfigurableModuleBuilder } from "@nestjs/common";
-import { randomUUID } from "crypto";
 
 import { type AuthModuleOptions } from "./interfaces";
 
-export const PERMISSIONS_METADATA_KEY = randomUUID();
-
-export const REQUIRE_AUTH_METADATA_KEY = randomUUID();
-
 export const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN } =
   new ConfigurableModuleBuilder<AuthModuleOptions>().build();
-
-export const AUTH_USER = Symbol("AUTH_USER");
-
-export const AUTH_PERSONAL_ACCESS_TOKEN = Symbol("AUTH_PERSONAL_ACCESS_TOKEN");
