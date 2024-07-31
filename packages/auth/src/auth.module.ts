484f3496d688fc1c8e8858464c6bbb63dd90f67f
Xudong Huang <me@huangxudong.com>
2024-07-31 17:18:01 +0800

    feat: 使用 dataloader 来解决 GraphQL 认证时多次读取数据库。 (#126)

--- packages/auth/src/auth.module.ts
+++ packages/auth/src/auth.module.ts
@@ -1,21 +1,10 @@
-import {
-  REQUEST,
-  RequestContext,
-  RequestContextModule,
-} from "@nest-boot/request-context";
-import { Inject, Module, OnModuleInit, Type } from "@nestjs/common";
+import { RequestContextModule } from "@nest-boot/request-context";
+import { Module } from "@nestjs/common";
 import { APP_GUARD, Reflector } from "@nestjs/core";
-import { Request } from "express";
 
-import { AUTH_PERSONAL_ACCESS_TOKEN, AUTH_USER } from "./auth.constants";
 import { AuthGuard } from "./auth.guard";
-import {
-  ConfigurableModuleClass,
-  MODULE_OPTIONS_TOKEN,
-} from "./auth.module-definition";
+import { ConfigurableModuleClass } from "./auth.module-definition";
 import { AuthService } from "./auth.service";
-import { PersonalAccessToken, User } from "./entities";
-import { AuthModuleOptions } from "./interfaces";
 
 @Module({
   imports: [RequestContextModule],
@@ -29,84 +18,4 @@ import { AuthModuleOptions } from "./interfaces";
   ],
   exports: [AuthService],
 })
-export class AuthModule
-  extends ConfigurableModuleClass
-  implements OnModuleInit
-{
-  constructor(
-    private readonly authService: AuthService,
-    @Inject(MODULE_OPTIONS_TOKEN)
-    private readonly options: AuthModuleOptions,
-  ) {
-    super();
-
-    this.defaultRequireAuth = this.options?.defaultRequireAuth ?? true;
-
-    this.userEntityClass = this.options.entities?.User ?? User;
-    this.personalAccessTokenEntityClass =
-      this.options.entities?.PersonalAccessToken ?? PersonalAccessToken;
-  }
-
-  onModuleInit() {
-    RequestContext.registerMiddleware(
-      "auth",
-      async (ctx, next) => {
-        if (ctx.type === "http") {
-          const req = ctx.get<Request>(REQUEST);
-
-          if (req) {
-            const token = this.extractPersonalAccessToken(req);
-
-            if (token) {
-              const personalAccessToken =
-                await this.authService.getToken(token);
-              const user = await personalAccessToken?.user.load();
-
-              if (personalAccessToken && user) {
-                RequestContext.set(this.userEntityClass as Type<User>, user);
-                RequestContext.set(
-                  this
-                    .personalAccessTokenEntityClass as Type<PersonalAccessToken>,
-                  personalAccessToken,
-                );
-
-                RequestContext.set(AUTH_USER, user);
-                RequestContext.set(
-                  AUTH_PERSONAL_ACCESS_TOKEN,
-                  personalAccessToken,
-                );
-
-                await this.authService.updateLastUsedAt(personalAccessToken);
-              }
-            }
-          }
-        }
-
-        return await next();
-      },
-      ["database"],
-    );
-  }
-
-  /**
-   * Extracts the personal access token from the request.
-   * @param req - The request object.
-   * @returns The personal access token, or null if it doesn't exist.
-   */
-  private extractPersonalAccessToken(req: Request): string | null {
-    const authorizationHeader = req.get("authorization");
-    if (typeof authorizationHeader !== "undefined") {
-      const matched = authorizationHeader.match(/(\S+)\s+(\S+)/);
-
-      if (matched !== null && matched[1].toLowerCase() === "bearer") {
-        return matched[2];
-      }
-    }
-
-    if (typeof req.cookies?.token === "string") {
-      return req.cookies.token;
-    }
-
-    return null;
-  }
-}
+export class AuthModule extends ConfigurableModuleClass {}
