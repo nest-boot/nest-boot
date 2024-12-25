1a59e0136688545788b3153dc3e54161f7bada68
Xudong Huang <me@huangxudong.com>
2024-12-25 14:40:30 +0800

    fix: Replace DiscoveryService and MetadataScanner with DiscoveryModule in QueueCoreModule and ScheduleModule
    - Updated imports in QueueCoreModule and ScheduleModule to use DiscoveryModule instead of DiscoveryService and MetadataScanner.
    - Simplified provider lists by removing unnecessary services, enhancing module clarity and maintainability.

--- packages/queue/src/queue-core.module.ts
+++ packages/queue/src/queue-core.module.ts
@@ -1,6 +1,6 @@
 import { RequestContextModule } from "@nest-boot/request-context";
 import { Global, Logger, Module } from "@nestjs/common";
-import { DiscoveryService, MetadataScanner } from "@nestjs/core";
+import { DiscoveryModule } from "@nestjs/core";
 
 import { JobEntityService } from "./job-entity.service";
 import { QueueExplorer } from "./queue.explorer";
@@ -8,15 +8,8 @@ import { QueueManager } from "./queue.manager";
 
 @Global()
 @Module({
-  imports: [RequestContextModule],
-  providers: [
-    DiscoveryService,
-    MetadataScanner,
-    Logger,
-    QueueExplorer,
-    QueueManager,
-    JobEntityService,
-  ],
+  imports: [RequestContextModule, DiscoveryModule],
+  providers: [Logger, QueueExplorer, QueueManager, JobEntityService],
   exports: [QueueExplorer, QueueManager],
 })
 export class QueueCoreModule {}
