5e2e7aa54a1ee03135092aafee8b1ea7483327ae
Xudong Huang <me@huangxudong.com>
2026-09-19 11:13:49 -0700

    fix(graphql-connection): accept null optional arguments

--- packages/graphql-connection/src/connection-query-builder.ts
+++ packages/graphql-connection/src/connection-query-builder.ts
@@ -174,7 +174,7 @@ export class ConnectionQueryBuilder<
   }
 
   private getQueryOrderMap(): QueryOrderMap<Entity>[] {
-    if (typeof this.args.orderBy === "undefined") {
+    if (this.args.orderBy == null) {
       return [
         {
           id: this.queryOrder,
@@ -218,7 +218,7 @@ export class ConnectionQueryBuilder<
           } as unknown as FilterQuery<Entity>)
         : null;
 
-    return typeof this.args.orderBy !== "undefined" &&
+    return this.args.orderBy != null &&
       typeof this.cursor?.value !== "undefined"
       ? ({
           $or: [
@@ -241,10 +241,7 @@ export class ConnectionQueryBuilder<
   }
 
   private getQueryStringToFilterQuery(): FilterQuery<Entity> | null {
-    if (
-      typeof this.args.query !== "undefined" &&
-      this.args.query.trim() !== ""
-    ) {
+    if (this.args.query != null && this.args.query.trim() !== "") {
       const { fieldOptionsMap, filterQuerySchema } = this.metadata;
 
       // Build ParseOptions, only include filterable fields
@@ -360,7 +357,7 @@ export class ConnectionQueryBuilder<
       node: node as Entity,
       cursor: new Cursor({
         id: (node as any)?.id,
-        ...(typeof this.args.orderBy !== "undefined"
+        ...(this.args.orderBy != null
           ? { value: get(node, this.args.orderBy.field) }
           : {}),
       }).toString(),
