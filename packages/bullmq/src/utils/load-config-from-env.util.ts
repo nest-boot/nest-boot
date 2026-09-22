9aecc98d77eae769c10ec7bfd90951b835f66496
Xudong Huang <me@huangxudong.com>
2026-09-22 02:53:01 -0700

    refactor(redis): share URL connection parsing across modules

--- packages/bullmq/src/utils/load-config-from-env.util.ts
+++ packages/bullmq/src/utils/load-config-from-env.util.ts
@@ -1,28 +1,6 @@
+import { parseRedisUrl } from "@nest-boot/redis/connection-options";
 import { ConnectionOptions } from "bullmq";
 
-function normalizeHostname(hostname: string): string {
-  if (hostname.startsWith("[") && hostname.endsWith("]")) {
-    return hostname.slice(1, -1);
-  }
-
-  return hostname;
-}
-
 export function loadConfigFromEnv(): ConnectionOptions {
-  if (process.env.REDIS_URL) {
-    const url = new URL(process.env.REDIS_URL);
-    const port = url.port;
-    const database = url.pathname.split("/")[1];
-
-    return {
-      host: normalizeHostname(url.hostname),
-      port: port ? +port : undefined,
-      db: database ? +database : undefined,
-      username: decodeURIComponent(url.username),
-      password: decodeURIComponent(url.password),
-      ...(url.protocol === "rediss:" ? { tls: {} } : {}),
-    };
-  }
-
-  return {};
+  return process.env.REDIS_URL ? parseRedisUrl(process.env.REDIS_URL) : {};
 }
--- packages/redis/src/index.ts
+++ packages/redis/src/index.ts
@@ -1,2 +1,3 @@
+export * from "./connection-options.js";
 export * from "./redis.module.js";
 export * from "./utils/load-config-from-env.util.js";
